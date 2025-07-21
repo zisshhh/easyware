34424d56782c71a0e3592817b7e909bc626739de
zisshhh <zisshhh100xdevs@gmail.com>
2025-07-22 00:37:11 +0530

    initialize project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Signup } from './pages/Signup'
+
+
+function App() {
+
+  return (
+    <div>
+      <Signup />
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/Signup.tsx
@@ -0,0 +1,9 @@
+import { Input } from "@/components/ui/input"
+
+export const Signup = () => {
+    return (
+        <div>
+            <Input type="email" placeholder="zisshh@email.com" />
+        </div>
+    )
+}
